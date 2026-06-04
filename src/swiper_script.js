a47ad79111c724e750f24539f7388154ab75d9e8
jan wiener <jan.wiener.024@pslib.cz>
2026-06-04 09:38:05 +0200

    Content additions

--- src/swiper_script.js
+++ src/swiper_script.js
@@ -6,11 +6,11 @@ import 'swiper/css/bundle';
 
 
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   speed: 200,
   centeredSlides: true,
   loop: true,
-  effect: "flip",
+  effect: "slide",
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
